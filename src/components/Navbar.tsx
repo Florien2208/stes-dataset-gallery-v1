7388a6abfd694eb30a99f3ed51276c545baddb9e
florien2208 <mahorompakanyiflorien@gmail.com>
2024-09-04 10:02:41 +0200

    updates on gallery

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-blue-500 p-4 text-white">
-      <h1 className="text-2xl font-bold">Gallery Dashboard</h1>
+      <h1 className="text-2xl font-bold">Gallery Collection</h1>
     </nav>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,26 +5,23 @@ interface SidebarProps {
   onCreateCollection: () => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ onCreateCollection }) => {
+const Sidebar: React.FC<SidebarProps> = () => {
   return (
     <aside className="w-64 bg-gray-800 text-white p-6">
       <ul>
-        <Link to="gallery" ><li className="mb-4">Home</li></Link>
-          <Link
-            to="/"
-            className="mb-4 cursor-pointer"
-            onClick={onCreateCollection}
-          >
-        <li>
-            Manage Gallery
-        </li>
-          </Link>
-          <Link to="create-collection" className="mb-4">
-        <li>
-          
-            Create Collection
-        </li>
-          </Link>
+        <Link to="gallery">
+          <li className="mb-4">Manage Gallery</li>
+        </Link>
+        {/* <Link
+          to="/"
+          className="mb-4 cursor-pointer"
+          onClick={onCreateCollection}
+        >
+          <li>Manage Gallery</li>
+        </Link> */}
+        <Link to="create-collection" className="mb-4">
+          <li>Create Collection</li>
+        </Link>
       </ul>
     </aside>
   );
